f2d3ce5e342defca0ef90788e661d10a492ad589
Angelika Hoeflich <angelikahoeflich@Angelikas-MacBook-Air.local>
2020-10-21 14:34:55 -0700

    how to filter through array in text input

--- app-3/src/App.js
+++ app-3/src/App.js
@@ -1,7 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+// import logo from './logo.svg';
 import './App.css';
 
 
+class App extends Component {
+  constructor(){
+    super()
+
+    this.state = {
+      filteredArr: '',
+      hobbies: ['coding ', 'cooking ', 'working out ', 'stressing out']
+    }
+
+  }
+  handleChange(filter){
+    this.setState({
+      filteredArr:filter
+    });
+  }
+
+  render(){
+    let hobbies = this.state.hobbies
+      .filter((e) => {
+        return e.includes(this.state.filteredArr)
+      })
+      .map((e)=> {
+        return <h2 key={e}>{e}</h2>
+      });
+
+      return(
+        <div className ='App'>
+          <input onChange={e=> this.handleChange(e.target.value)}/>
+          <p>{hobbies}</p>
+        </div>
+      );
+    }
+}
 
 export default App;
