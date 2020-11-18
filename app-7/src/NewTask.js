9b80a22596fd2a17180c9aa5a4b6493ebd1aa140
Angelika Hoeflich <angelikahoeflich@Angelikas-MacBook-Air.local>
2020-11-17 17:21:55 -0800

    setting up component blueprint

---
+++ app-7/src/NewTask.js
@@ -0,0 +1,27 @@
+import React, {Component} from 'react';
+
+class NewTask extends Component{
+  constructor(){
+    super()
+
+    this.state = {
+      input: ''
+    }
+
+    this.handleAdd = this.handleAdd.bind(this)
+  }
+
+
+
+  render(){
+    return(
+      <div>
+
+        
+      </div>
+    )
+  }
+
+}
+
+export default NewTask
