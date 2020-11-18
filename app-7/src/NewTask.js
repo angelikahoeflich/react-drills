cd5ac96c2bee486283b630e2dc340603b4d06e37
Angelika Hoeflich <angelikahoeflich@Angelikas-MacBook-Air.local>
2020-11-17 17:25:29 -0800

    newTask component finished

--- app-7/src/NewTask.js
+++ app-7/src/NewTask.js
@@ -12,12 +12,30 @@ class NewTask extends Component{
   }
 
 
+  handleInputChange(value){
+    this.setState({
+      input:value
+    })
+  }
+
+  handleAdd(){
+    this.props.add(this.state.input)
+    this.setState({
+      input: ''
+    })
+  }
 
   render(){
     return(
       <div>
 
-        
+        <input 
+            type="text"
+            value={this.state.input}
+            placeholder="Enter Task"
+            onChange={e => this.handleInputChange(e.target.value)}
+          />
+          <button onClick={this.handleAdd}>Add Task</button>
       </div>
     )
   }
